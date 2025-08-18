5345468d1f15a1c1022b4fc2241562ca068076f8
Koteykee <koteykeethecat@gmail.com>
2025-08-18 11:55:52 +0300

    Base

---
+++ src/components/PostsList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>POSTS LIST</div>
+  <PostsListItem />
+  <PostsListItem />
+  <PostsListItem />
+</template>
+
+<script setup>
+import PostsListItem from "./PostsListItem.vue";
+</script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>HOME</div>
+  <PostsList />
+</template>
+
+<script setup>
+import PostsList from "@/components/PostsList.vue";
+</script>
+
+<style scoped></style>
